ccbf6926e92711be4d97e68a08ed9e36475f9d54
PetrasVilkelis <petrasvilkelis@gmail.com>
2020-10-06 14:43:00 +0200

    Solve all exercises

---
+++ day-7/ex.js
@@ -0,0 +1,152 @@
+/*EXERCISE 1 
+     Create a new welcome alert message when the page successfully loads
+    */
+// alert("Welcome!");
+/* EXERCISE 2
+      Write a function to change the Title of the page in something else (execute the function in browser console)
+  */
+const changeTitle = function (newTitle) {
+  document.title = newTitle;
+};
+/* EXERCISE 3a
+      Write a function to add a class to the page's h1 title in "red-color" (execute the function when title is hovered by mouse).
+  */
+const addClassToTitle = function () {
+  const heading = document.querySelector("h1");
+  heading.classList.add("red-color");
+};
+/* EXERCISE 3b
+      Write a function to remove "red-color" class from previous h1 (execute the function when the mouse leaves the title).
+  */
+const removeClassFromTitle = function () {
+  const heading = document.querySelector("h1");
+  heading.classList.remove("red-color");
+};
+/* EXERCISE 4
+      Add the following html snippet to your page:
+          <h2 id="listTitle">Todo List</h2>
+          <ul id="firstList">
+              <li><p>1st</p></li>
+              <li>2nd</li>
+              <li>3rd</li>
+          </ul>
+          <ul id="secondList">
+              <li>1st</li>
+              <li>2nd</li>
+              <li>3rd</li>
+          </ul>
+          <div>
+              <p>This text is just for the exercise</p>
+          </div>
+  Write a function to change the text of only the p which are child of a div (execute the function by assigning it to a button's click event)
+  */
+const changePContent = function () {
+  const p = document.querySelector("div>p");
+
+  p.innerText = "Hello!";
+};
+/* EXERCISE 5
+      Write a function to change the list title (you can use previous day's textarea as input or create a new text input field to grab the content)
+  */
+const changeListTitle = function () {
+  const input = document.querySelector(
+    "#titleInput"
+  );
+  const heading = document.querySelector(
+    "#listTitle"
+  );
+
+  heading.innerText = input.value;
+};
+/* EXERCISE 6
+       Write a function to add a new item ONLY to the second list (create an input field + add button)
+  */
+const addToTheSecond = function (content) {
+  const secondList = document.querySelector(
+    "#secondList"
+  );
+  const input = document.querySelector(
+    ".todoForm>input"
+  );
+
+  const el = document.createElement("li");
+
+  el.innerHTML = input.value;
+
+  secondList.append(el);
+};
+/* EXERCISE 7
+      Write a function to make the first UL disappear (button)
+  */
+const firstUlDisappear = function () {
+  document.querySelector("#firstList").remove();
+};
+/* EXERCISE 8
+      Write a function to make the background of every UL green (button)
+  */
+const paintItGreen = function () {
+  const uls = document.querySelectorAll(
+    "#firstList,#secondList"
+  );
+  uls[0].style.backgroundColor = "green";
+  uls[1].style.backgroundColor = "green";
+};
+
+/* EXERCISE 9
+      Add a "magnifier function" to the table.
+      When the user mouse goes on a table cell (not the image one) the font size must increase.
+      HINT use mouseenter / mouseleave events
+  */
+const makeThemMagnifiable = function () {
+  const tds = document.querySelectorAll(
+    "td:not(:first-child)"
+  );
+
+  for (const td of tds) {
+    td.addEventListener("mouseenter", () => {
+      td.style.fontSize = "2rem";
+    });
+    td.addEventListener("mouseleave", () => {
+      td.style.fontSize = "1rem";
+    });
+  }
+};
+/* EXERCISE 10
+      Add a button to toggle all the product images (toggle => if visible, hide, if not visible, show)
+  */
+const toggleShowImages = function () {
+  const imageParents = document.querySelectorAll(
+    "td:first-child"
+  );
+
+  let opacity = "";
+
+  if (imageParents[0].style.opacity === "0") {
+    opacity = 1;
+  } else {
+    opacity = 0;
+  }
+
+  for (const imageParent of imageParents) {
+    imageParent.style.opacity = opacity;
+  }
+};
+
+//##### EXTRA
+
+/* EXERCISE 11
+      Make the heading of the page change color radomly every time the user clicks on it
+      */
+const makeItClickable = function () {
+  const heading = document.querySelector("h1");
+  console.log(heading);
+  heading.style.backgroundColor =
+    "#" +
+    Math.floor(Math.random() * 16777215).toString(
+      16
+    );
+};
+
+/*EXERCISE 12
+    Refactor your HTML code with HTML5 semantic tags such as header, section, footer, etc
+*/
---
+++ day-7/main.js
@@ -0,0 +1,128 @@
+const products = [
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41hk96kbfVL._SS47_.jpg",
+    title: "Skechers Work Cessnock",
+    price: 39.99,
+    description:
+      "Skechers for work, cessnock food service shoe. Slip reisitant outsole.",
+    url:
+      "https://www.amazon.com/Skechers-Mens-Cessnock-Shoe-Black/dp/B07FBJ8CT1/ref=sr_1_4?dchild=1&keywords=shoes&qid=1601900245&sr=8-4",
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41tVc8w0pbL._SS47_.jpg",
+    title: "Skechers Equalizer 3.0",
+    price: 50.82,
+    description:
+      "Lace-up jogger with air cooled memory foam.",
+    url:
+      "https://www.amazon.com/Skechers-Relaxed-Equalizer-Sneakers-Black/dp/B07CTSYGZK/ref=pd_sbs_309_1/143-5812864-5363220?_encoding=UTF8&pd_rd_i=B07CTSYGZK&pd_rd_r=7ff54c2e-0b23-4b36-80e0-e5a7518cd3ab&pd_rd_w=3qTTb&pd_rd_wg=DJx2f&pf_rd_p=b65ee94e-1282-43fc-a8b1-8bf931f6dfab&pf_rd_r=QQW82D76B4R85SE3G20P&psc=1&refRID=QQW82D76B4R85SE3G20P",
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/5165dpKCJ1L._SS47_.jpg",
+    title:
+      "Mens Air Athletic Running Tennis Shoes",
+    price: 39.99,
+    description:
+      "Fashion knitted mesh upper for ultra-lightweight support and breathable, allows to keep your feet dry while exercising.",
+    url:
+      "https://www.amazon.com/dp/B089RB117S/ref=sspa_dk_detail_8?pd_rd_i=B089RB117S&pd_rd_w=SN5Er&pf_rd_p=f0355a48-7e73-489a-9590-564e12837b93&pd_rd_wg=pg6xB&pf_rd_r=2EPZT8M6HDFT97FTK32P&pd_rd_r=da0f3bf3-cfcb-4678-96d1-85cff1c22af0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWDhMRlpQTEUwRFQ5JmVuY3J5cHRlZElkPUEwNDY5MzU3M0E3VjNRTzVYODdQRSZlbmNyeXB0ZWRBZElkPUEwNzA5NjUzMTcwVjRWN08zV1I5TiZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1&psc=1",
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41hk96kbfVL._SS47_.jpg",
+    title:
+      "Skechers Men's Parson-Todrick Sneaker",
+    price: 63.99,
+    description: "Knitted Bungee Mesh Slip On.",
+    url:
+      "https://www.amazon.com/dp/B07M6JWDWP/ref=sspa_dk_detail_9?psc=1&pd_rd_i=B07M6JWDWP&pd_rd_w=SN5Er&pf_rd_p=f0355a48-7e73-489a-9590-564e12837b93&pd_rd_wg=pg6xB&pf_rd_r=2EPZT8M6HDFT97FTK32P&pd_rd_r=da0f3bf3-cfcb-4678-96d1-85cff1c22af0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWDhMRlpQTEUwRFQ5JmVuY3J5cHRlZElkPUEwNDY5MzU3M0E3VjNRTzVYODdQRSZlbmNyeXB0ZWRBZElkPUEwNzc3NTQ4MUs2SkhMNVQxWDNDNiZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41wRSfcKNNL._SS47_.jpg",
+    title:
+      "Skechers Men's Skech Flex 2.0 Milwee Fashion Sneaker",
+    price: 80.75,
+    description:
+      "Lace-up with air bag midsole and air cooled memory foam insole",
+    url:
+      "https://www.amazon.com/dp/B06XYVMJFK/ref=sspa_dk_detail_7?psc=1&pd_rd_i=B06XYVMJFK&pd_rd_w=SN5Er&pf_rd_p=f0355a48-7e73-489a-9590-564e12837b93&pd_rd_wg=pg6xB&pf_rd_r=2EPZT8M6HDFT97FTK32P&pd_rd_r=da0f3bf3-cfcb-4678-96d1-85cff1c22af0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWDhMRlpQTEUwRFQ5JmVuY3J5cHRlZElkPUEwNDY5MzU3M0E3VjNRTzVYODdQRSZlbmNyeXB0ZWRBZElkPUEwNTUxOTYxRVpNRlVESktOWExFJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+  },
+];
+
+const table = document.querySelector(".table");
+const btn = document.querySelector(".btn");
+const input = document.querySelector("input");
+
+btn.addEventListener("click", sendMessage);
+
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    sendMessage();
+  }
+});
+
+for (let product of products) {
+  const tr = document.createElement("tr");
+
+  tr.innerHTML = `
+  <td>
+    <img
+      src="${product.image}"
+      alt="Product image"
+    />
+    </td>
+  </td>
+  <td>
+    ${product.title}
+  </td>
+  <td>
+    ${product.description}
+  </td>
+  <td>$${product.price}</td>
+  <td>
+    <a href="${product.url}"
+      >Buy</a
+    >
+  </td>`;
+
+  table.appendChild(tr);
+}
+
+function changeHeaderText(text) {
+  const header = document.querySelector(".title");
+  header.innerText = text;
+}
+
+function changeBackgroundColor(color) {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = color;
+}
+
+function changeFooterAddress(fakeAddress) {
+  const address = document.querySelector(
+    ".address"
+  );
+  address.innerText = fakeAddress;
+}
+
+function sendMessage() {
+  if (!input.value) return;
+
+  const parent = document.querySelector(
+    ".messages"
+  );
+
+  const li = document.createElement("li");
+
+  li.classList = "message";
+  li.innerText = input.value;
+
+  parent.appendChild(li);
+
+  input.value = "";
+}
